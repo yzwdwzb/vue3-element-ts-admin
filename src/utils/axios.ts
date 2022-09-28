7af65f4372b3a575fa541e29efac3d722d7cb2f9
wangzb <1196218720@qq.com>
2022-09-28 11:45:01 +0800

    test: 添加模拟数据

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,4 +2,20 @@ import axios from 'axios'
 
 axios.defaults.baseURL = 'http://localhost:3004';
 
+axios.defaults.timeout = 8000;
+
+//设置post请求头
+axios.defaults.headers.post['Content-Type'] = 'Content-Type: application/json';
+
+// 响应拦截器
+axios.interceptors.response.use(    
+    response => {   
+        if (response.status === 200) {            
+            return Promise.resolve(response.data);        
+        } else {       
+            return Promise.reject(response);        
+        }    
+    },     
+);
+
 export default axios
